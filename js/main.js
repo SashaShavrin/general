e6692e86e76c510e22f7308fab79ce498e303d08
Sasha <97390534+SashaShavrin@users.noreply.github.com>
2022-11-11 17:17:20 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -110,30 +110,57 @@
   }
 
 
-  
-  function getImg() {
-    let img = document.getElementById('block1');
-    img.classList.toggle('show')
+  if (window.innerWidth < 500) {
+    function getImg() {
+      let img = document.getElementById('block1');
+      img.classList.toggle('show')
+    }
+    setTimeout(getImg, 2000)
+    
+    function getFullName() {
+      let fullName = document.getElementById('block2');
+      fullName.classList.toggle('show')
+    }
+    setTimeout(getFullName,2100)
+    
+    function getText() {
+      let text = document.getElementById('block3');
+      text.classList.toggle('show')
+    }
+    setTimeout(getText, 2200)
+    
+    function getMenu() {
+      let menu = document.getElementById('menu');
+      menu.classList.toggle('showMenu')
+    }
+    setTimeout(getMenu, 1000)
+  } else {
+    function getImg() {
+      let img = document.getElementById('block1');
+      img.classList.toggle('show')
+    }
+    setTimeout(getImg, 1400)
+    
+    function getFullName() {
+      let fullName = document.getElementById('block2');
+      fullName.classList.toggle('show')
+    }
+    setTimeout(getFullName, 1500)
+    
+    function getText() {
+      let text = document.getElementById('block3');
+      text.classList.toggle('show')
+    }
+    setTimeout(getText, 1600)
+    
+    function getMenu() {
+      let menu = document.getElementById('menu');
+      menu.classList.toggle('showMenu')
+    }
+    setTimeout(getMenu, 1000)
   }
-  setTimeout(getImg, 1400)
   
-  function getFullName() {
-    let fullName = document.getElementById('block2');
-    fullName.classList.toggle('show')
-  }
-  setTimeout(getFullName, 1500)
-  
-  function getText() {
-    let text = document.getElementById('block3');
-    text.classList.toggle('show')
-  }
-  setTimeout(getText, 1600)
-  
-  function getMenu() {
-    let menu = document.getElementById('menu');
-    menu.classList.toggle('showMenu')
-  }
-  setTimeout(getMenu, 1000)
+ 
 
   var hello = "Hello! My name is Alexander";
   var i = -1;
