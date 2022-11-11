f7f0fd4ec187b56f7af51a7af313d25d2ff1a607
Sasha <97390534+SashaShavrin@users.noreply.github.com>
2022-11-11 14:46:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,14 @@
       }, 3000 * i)
     }
   }())
-  
+
+
+   function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
+
  
   window.onscroll = function() {
     var scrolled = window.pageYOffset;
