83d7d114e82a02427557dfcace12175609329d5e
Sasha <97390534+SashaShavrin@users.noreply.github.com>
2022-11-14 20:39:42 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,12 +9,6 @@
   }
 }())
 
-function menuOnClick() {
-  document.getElementById("menu-bar").classList.toggle("change");
-  document.getElementById("nav").classList.toggle("change");
-  document.getElementById("menu-bg").classList.toggle("change-bg");
-}
-
 window.onscroll = function() {
   var scrolled = window.pageYOffset;
   if (scrolled > 500) {
