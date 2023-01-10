b59fd2674164b4dc6ffc735949233570a2e51819
Sasha <97390534+SashaShavrin@users.noreply.github.com>
2023-01-10 11:17:14 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ if (window.innerWidth < 500) {
     let img = document.getElementById('block1');
     img.classList.toggle('show')
   }
-  setTimeout(getImg, 1500)
+  setTimeout(getImg, 2000)
 
   function getFullName() {
     let fullName = document.getElementById('block2');
     fullName.classList.toggle('show')
   }
-  setTimeout(getFullName, 1600)
+  setTimeout(getFullName, 2100)
 
   function getText() {
     let text = document.getElementById('block3');
     text.classList.toggle('show')
   }
-  setTimeout(getText, 1700)
+  setTimeout(getText, 2200)
 
   function getMenu() {
     let menu = document.getElementById('menu');
@@ -27,19 +27,19 @@ if (window.innerWidth < 500) {
     let img = document.getElementById('block1');
     img.classList.toggle('show')
   }
-  setTimeout(getImg, 1200)
+  setTimeout(getImg, 1400)
 
   function getFullName() {
     let fullName = document.getElementById('block2');
     fullName.classList.toggle('show')
   }
-  setTimeout(getFullName, 1300)
+  setTimeout(getFullName, 1500)
 
   function getText() {
     let text = document.getElementById('block3');
     text.classList.toggle('show')
   }
-  setTimeout(getText, 1400)
+  setTimeout(getText, 1600)
 
   function getMenu() {
     let menu = document.getElementById('menu');
