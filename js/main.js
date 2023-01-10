1bbb2772f185aad4b0493dae844e0bfd3adc00c3
Sasha <97390534+SashaShavrin@users.noreply.github.com>
2023-01-10 11:45:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+let hello = "Hello! I am Alexander";
+let i = -1;
+if (window.innerWidth < 500) {
+  setInterval(function() {
+    if (i >= hello.length - 1) {
+      return false
+    } else {
+      ++i
+      document.getElementById('insertHello').innerHTML += hello[i];
+      insertHello.classList.toggle('sayHello')
+    }
+  }, 15)
+} else {
+  setInterval(function() {
+    if (i >= hello.length - 1) {
+      return false
+    } else {
+      ++i
+      document.getElementById('insertHello').innerHTML += hello[i];
+      insertHello.classList.toggle('sayHello')
+    }
+  }, 30)
+}
+
+
 if (window.innerWidth < 500) {
   function getImg() {
     let img = document.getElementById('block1');
